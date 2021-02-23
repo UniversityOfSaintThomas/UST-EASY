41b87e08fa058a19f09829479c309262d5b53998
Thaddaeus Dahlberg <tcdahlberg@users.noreply.github.com>
2021-02-23 15:20:11 -0600

    widgets!

--- force-app/main/default/staticresources/EasyAppResources/js/scripts.js
+++ force-app/main/default/staticresources/EasyAppResources/js/scripts.js
@@ -49,16 +49,138 @@ ready(() => {
         field.setAttribute('disabled', 'disabled');
     });
 
-    document.querySelectorAll(".slds-select_container span select").forEach(field => {
-        field.classList.add('slds-select');
-        alert(field.classList);
+    let dependentPicklists = document.querySelector(".slds-select_container .slds-dependent-picklist select");
+
+    document.querySelectorAll('.dependent-picklist').forEach(pl => {
+        pl.querySelector('select').classList.add('slds-select');
+        observer.observe(
+            pl,
+            {
+                attributes: true,
+                characterData: true,
+                childList: true,
+                subtree: true,
+                attributeOldValue: true,
+                characterDataOldValue: true
+            }
+        );
+
+        // Notify us when any node within the document is modified, added removed, etc.
+        pl.addEventListener("DOMSubtreeModified", function(e) {
+            // Notify of change!
+            console.warn("change!", e);
+        }, false);
+
+
     });
+
+
+
 
 });
 
-const observer = new MutationObserver(mutation => {
-    document.querySelectorAll(".slds-select_container span select").forEach(field => {
-        field.classList.add('slds-select');
+let observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        console.log(mutation);
     });
-    console.log('DOM mutation detected');
 });
+
+// // Notify us when any node within the document is modified, added removed, etc.
+// document.addEventListener("DOMSubtreeModified", function(e) {
+//     // Notify of change!
+//     console.warn("change!", e);
+// }, false);
+
+
+function appShowLoadingSpinner() {
+    document.getElementById("apploadingspinner").style.display = "block";
+}
+
+function appHideLoadingSpinner() {
+    document.getElementById("apploadingspinner").style.display = "none";
+}
+
+function appShowConfimation() {
+    document.getElementById("confirmation").style.display = "block";
+}
+
+// By default, replaceAll runs on ALL textarea fields.
+// modified to run only on the class defined 'ckeditor'
+//CKEDITOR.replaceAll = function(){for(var a=document.getElementsByClassName("ckeditor"),b=0;b<a.length;b++){var d=null,k=a[b];if(k.name||k.id){if("string"==typeof arguments[0]){if(!(new RegExp("(?:^|\\s)"+arguments[0]+"(?:$|\\s)")).test(k.className))continue}else if("function"==typeof arguments[0]&&(d={},!1===arguments[0](k,d)))continue;this.replace(k,d)}}};
+
+// destroys and rebuilds the rich text fields after rerender
+function afterRerenderRTF() {
+    for (name in CKEDITOR.instances) {
+        delete CKEDITOR.instances[name];
+    }
+
+    CKEDITOR.replaceAll('ckeditor');
+}
+
+// I use this before save as I've observed situations where changes are lost,
+// particularly after the fields have already been rerendered.
+// More of a safeguard than anything
+function ensureRichTextContent() {
+
+    document.querySelectorAll('.ckeditor').forEach(function (el) {
+        let id = jQuery(el).attr('id') || jQuery(el).attr('name');
+        let data = CKEDITOR.instances[id].getData().trim();
+
+        if (CKEDITOR.instances[id].checkDirty()) {
+            el.textContent = data;
+        }
+
+    });
+}
+
+function performDocUploadSave(redirectTo) {
+    let docUploadPromiseArr = [];
+    ensureRichTextContent();
+
+    document.querySelectorAll('.docUploadInput').forEach(function (el, docUpload) {
+        if (docUpload.files) {
+            let fbody = docUpload.files[0];
+            if (fbody) {
+                docUploadPromiseArr.push(getAsText(fbody, jQuery(docUpload).attr('data-respid')));
+            }
+        } else {
+        }
+
+    });
+
+    document.querySelectorAll('.docUploadInput').forEach(function (idx, docUpload) {
+        if (docUpload.files) {
+            let fbody = docUpload.files[0];
+            if (fbody) {
+                docUploadPromiseArr.push(getAsText(fbody, docUpload.getAttribute('data-respid')));
+            }
+        } else {
+        }
+    });
+
+    Promise.all(docUploadPromiseArr).then(docUploads => {
+        var docUploadObj = {};
+        docUploads.forEach(function (docUpload) {
+            docUploadObj[docUpload.itemId] = {"attData": docUpload};
+        });
+        saveWithDocs(JSON.stringify(docUploadObj), redirectTo);
+        //console.log(redirectTo);
+    }).catch(function () {
+    });
+}
+
+function getAsText(readFile, respId) {
+    return new Promise((resolve, reject) => {
+        //var reader = new FileReader();
+        let reader = new FileReader();
+        reader.onload = (function (theFile) {
+            var fileName = theFile.name;
+            return function (e) {
+                resolve({"fileName": fileName, "data": e.target.result, "itemId": respId});
+            };
+        })(readFile);
+        reader.readAsDataURL(readFile);
+    });
+}
+
+

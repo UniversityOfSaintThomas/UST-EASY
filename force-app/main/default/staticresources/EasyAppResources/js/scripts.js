b68fde6e101081e7db2a129ebdbe93b822d17cf9
Thaddaeus Dahlberg <tcdahlberg@users.noreply.github.com>
2021-04-08 11:41:05 -0500

    Save button working again, references not yet, styling

--- force-app/main/default/staticresources/EasyAppResources/js/scripts.js
+++ force-app/main/default/staticresources/EasyAppResources/js/scripts.js
@@ -9,35 +9,12 @@ let ready = (callback) => {
     else document.addEventListener("DOMContentLoaded", callback);
 }
 
-let placeholders = {};
-let maxLengths = {};
-
 ready(() => {
     pageLoadReRendered();
     activateCarousel();
 });
 
 function pageLoadReRendered() {
-    for (let inputId in placeholders) {
-        if (inputId) {
-            document.querySelectorAll('.' + inputId).forEach(field => {
-                field.setAttribute('aria-describedby', "error-message-" + inputId);
-                if (placeholders[inputId] && !field.classList.contains('field-TextEncrypted')) {
-                    field.setAttribute('placeholder', placeholders[inputId])
-                }
-            });
-        }
-    }
-
-    for (let inputId2 in maxLengths) {
-        if (inputId2) {
-            document.querySelectorAll('.' + inputId2).forEach(field => {
-                if (maxLengths[inputId2]) {
-                    field.setAttribute('maxlength', maxLengths[inputId2])
-                }
-            });
-        }
-    }
 
     document.querySelectorAll('.fieldNotEditable,.fieldNotEditable input,.fieldNotEditable select,.fieldNotEditable textarea').forEach(field => {
         field.setAttribute('disabled', 'disabled');
@@ -63,10 +40,32 @@ function pageLoadReRendered() {
             formatPhone(ph);
         });
     });
-
-    activateAutoComplete();
+    adjustLabelsFor();
     radioCheckBox();
+    activateAutoComplete();
+}
 
+function adjustLabelsFor() {
+    document.querySelectorAll('.slds-input').forEach(inputFound => {
+        let inputWrapper = inputFound.closest('.slds-form-element'),
+            inputLabel = inputWrapper.querySelector('label'),
+            helpText = inputWrapper.querySelector('.slds-form-element__help');
+
+        inputLabel.htmlFor = inputFound.getAttribute('id');
+        if (inputFound && helpText) {
+            if (helpText) {
+                inputFound.setAttribute('aria-describedby', helpText.getAttribute('id'));
+                inputFound.setAttribute('aria-invalid', 'false');
+            }
+            if (inputWrapper.dataset.placeholder) {
+                field.setAttribute('placeholder', placeholders[inputId])
+                inputFound.setAttribute('placeholder', inputWrapper.dataset.placeholder);
+            }
+            if (inputWrapper.dataset.maxlength) {
+                inputFound.setAttribute('maxlength', inputWrapper.dataset.maxlength);
+            }
+        }
+    });
 }
 
 function radioCheckBox() {
@@ -160,7 +159,6 @@ function performDocUploadSave(redirectTo) {
             docUploadObj[docUpload.itemId] = {"attData": docUpload};
         });
         saveWithDocs(JSON.stringify(docUploadObj), redirectTo);
-        //console.log(redirectTo);
     }).catch(function () {
     });
 }
@@ -233,10 +231,13 @@ function lookupResultsFormatter(data, originObjId) {
     let resultList = listObject.querySelector('.slds-listbox');
     let comboBox = originObject.closest('.slds-combobox');
     let fieldNames = originObject.dataset.objtypenamefield.replace(' ', '').split(',');
-    //console.log(JSON.stringify(data));
     data.forEach(result => {
         let resultName = '';
         let subTitle = '';
+        let resultId = '';
+        if (result['Id']) {
+            resultId = result['Id'];
+        }
         for (let x = 0; x < fieldNames.length; x++) {
             let fieldName = fieldNames[x].trim();
             if (x === 0) {
@@ -250,7 +251,7 @@ function lookupResultsFormatter(data, originObjId) {
         if (subTitle) {
             subTitle = subTitle.substr(0, subTitle.length - 2);
         }
-        outputList += resultListTemplate(resultName, subTitle, originObject.dataset.listicon, originObjId, result.Id);
+        outputList += resultListTemplate(resultName, subTitle, originObject.dataset.listicon, originObjId, resultId);
     });
     resultList.innerHTML = '';
     comboBox.classList.remove('slds-is-open');
@@ -276,33 +277,38 @@ function activateAutoComplete() {
 }
 
 function activateCarousel() {
-
     // Variables to target our base class,  get carousel items, count how many carousel items there are, set the slide to 0 (which is the number that tells us the frame we're on), and set motion to true which disables interactivity.
     const itemClassName = "carousel__item";
     let items = document.getElementsByClassName(itemClassName),
         totalItems = items.length,
         slide = 0,
-        moving = true;
+        moving = true,
+        saveButton = document.querySelector("[id$='groupSaveButton']"),
+        next = document.getElementsByClassName('carousel__button--next')[0],
+        prev = document.getElementsByClassName('carousel__button--prev')[0];
 
-    // To initialise the carousel we'll want to update the DOM with our own classes
     function setInitialClasses() {
-
-        // Target the last, initial, and next items and give them the relevant class.
-        // This assumes there are three or more items.
-        items[totalItems - 1].classList.add("prev");
         items[0].classList.add("active");
-        items[1].classList.add("next");
-
+        if (totalItems === 2) {
+            items[1].classList.add("next");
+        } else if (totalItems > 2) {
+            items[totalItems - 1].classList.add("prev");
+            items[1].classList.add("next");
+        }
     }
 
     // Set click events to navigation buttons
-
     function setEventListeners() {
-        let next = document.getElementsByClassName('carousel__button--next')[0],
-            prev = document.getElementsByClassName('carousel__button--prev')[0];
-
         next.addEventListener('click', moveNext);
         prev.addEventListener('click', movePrev);
+        if (totalItems === 1) {
+            next.style.display = 'none';
+            prev.style.display = 'none';
+            saveButton.style.dispay = 'inline-flex';
+        } else {
+            prev.style.display = 'none';
+            saveButton.style.display = "none";
+        }
     }
 
     // Disable interaction by setting 'moving' to true for the same duration as our transition (0.5s = 500ms)
@@ -314,95 +320,85 @@ function activateCarousel() {
     }
 
     function moveCarouselTo(slide) {
-
-        // Check if carousel is moving, if not, allow interaction
         if (!moving) {
-
-            // temporarily disable interactivity
             disableInteraction();
-
-            // Preemptively set variables for the current next and previous slide, as well as the potential next or previous slide.
             let newPrevious = slide - 1,
                 newNext = slide + 1,
                 oldPrevious = slide - 2,
                 oldNext = slide + 2;
 
-            //console.log('Total Items: ' + totalItems);
-            // Test if carousel has more than three items
-            //  if ((totalItems - 1) > 3) {
+            if (totalItems > 1) {
 
-            // Checks if the new potential slide is out of bounds and sets slide numbers
-            if (newPrevious <= 0) {
-                oldPrevious = (totalItems - 1);
-            } else if (newNext >= (totalItems - 1)) {
-                oldNext = 0;
+                if (newPrevious <= 0) {
+                    oldPrevious = (totalItems - 1);
+                } else if (newNext >= (totalItems - 1)) {
+                    oldNext = 0;
+                }
+
+                if (slide === 0) {
+                    newPrevious = (totalItems - 1);
+                    oldPrevious = (totalItems - 2);
+                    oldNext = (slide + 1);
+                } else if (slide === 1) {
+                    newPrevious = 0;
+                    oldPrevious = (totalItems - 1);
+                    oldNext = (slide + 1);
+                } else if (slide === (totalItems - 1)) {
+                    newPrevious = (slide - 1);
+                    newNext = 0;
+                    oldNext = 1;
+                }
+
+                if (slide + 1 === totalItems || totalItems === 1) {
+                    saveButton.style.display = "inline-flex"
+                    next.style.display = "none"
+                } else {
+                    saveButton.style.display = "none"
+                    next.style.display = "inline-flex"
+                }
+
+                if (slide > 0) {
+                    prev.style.display = "inline-flex"
+                } else {
+                    prev.style.display = "none"
+                }
+
+                items[oldPrevious].className = itemClassName;
+                items[oldNext].className = itemClassName;
+                items[newPrevious].className = itemClassName + " prev";
+                items[slide].className = itemClassName + " active";
+                items[newNext].className = itemClassName + " next";
             }
-
-            // Check if current slide is at the beginning or end and sets slide numbers
-            if (slide === 0) {
-                newPrevious = (totalItems - 1);
-                oldPrevious = (totalItems - 2);
-                oldNext = (slide + 1);
-            } else if (slide === (totalItems - 1)) {
-                newPrevious = (slide - 1);
-                newNext = 0;
-                oldNext = 1;
-            }
-
-            // Now we've worked out where we are and where we're going, by adding and removing classes, we'll be triggering the carousel's transitions.
-
-            // Based on the current slide, reset to default classes.
-            console.log('old previous: ' + oldPrevious + ', new previous: ' + newPrevious + ', active: ' + slide);
-
-            items[oldPrevious].className = itemClassName;
-            items[oldNext].className = itemClassName;
-
-            // Add the new classes
-            items[newPrevious].className = itemClassName + " prev";
-            items[slide].className = itemClassName + " active";
-            items[newNext].className = itemClassName + " next";
-            //   }
         }
     }
 
-    // Next navigation handler
     function moveNext() {
-        // Check if moving
         if (!moving) {
-            // If it's the last slide, reset to 0, else +1
             if (slide === (totalItems - 1)) {
                 slide = 0;
             } else {
                 slide++;
             }
-            // Move carousel to updated slide
             moveCarouselTo(slide);
         }
     }
 
-    // Previous navigation handler
     function movePrev() {
-        // Check if moving
         if (!moving) {
-            // If it's the first slide, set as the last slide, else -1
             if (slide === 0) {
                 slide = (totalItems - 1);
             } else {
                 slide--;
             }
-            // Move carousel to updated slide
             moveCarouselTo(slide);
         }
     }
 
-    // Initialise carousel
     function initCarousel() {
         setInitialClasses();
         setEventListeners();
-        // Set moving to false now that the carousel is ready
         moving = false;
     }
 
-    // make it rain
     initCarousel();
 }

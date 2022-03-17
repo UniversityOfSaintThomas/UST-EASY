9e8658061eb63c2b1958f6a1adceca6e3aaa65cf
Chase Ingebritson <chase@inge.me>
2022-03-17 01:34:36 -0500

    Created some test sites

--- force-app/main/default/lwc/rfiCmp/rfiCmp.js
+++ force-app/main/default/lwc/rfiCmp/rfiCmp.js
@@ -1,12 +1,15 @@
 import { LightningElement } from 'lwc';
-import FIRSTNAME_FIELD from '@salesforce/schema/Lead.FirstName';
-import LASTNAME_FIELD from '@salesforce/schema/Lead.LastName';
+import NAME_FIELD from '@salesforce/schema/Lead.Name';
 import EMAIL_FIELD from '@salesforce/schema/Lead.Email';
 import PHONE_FIELD from '@salesforce/schema/Lead.Phone';
-import MOBILEPHONE_FIELD from '@salesforce/schema/Lead.MobilePhone';
+// import MOBILEPHONE_FIELD from '@salesforce/schema/Lead.MobilePhone';
 import COMPANY_FIELD from '@salesforce/schema/Lead.Company';
 
 export default class RfiCmp extends LightningElement {
   objectApiName = 'Lead'
-  fields = [FIRSTNAME_FIELD, LASTNAME_FIELD, EMAIL_FIELD, PHONE_FIELD, MOBILEPHONE_FIELD, COMPANY_FIELD]
+
+  nameField = NAME_FIELD
+  emailField = EMAIL_FIELD
+  phoneField = PHONE_FIELD
+  companyField = COMPANY_FIELD
 }

3064319c24ebb5bd9d2ba4bcb59e67c409669143
nicole-dmass <nicole.b@digitalmass.com>
2022-06-02 09:40:14 -0500

    added dataset, lwc controller

--- force-app/main/default/lwc/requestForInformationForm/requestForInformationForm.js
+++ force-app/main/default/lwc/requestForInformationForm/requestForInformationForm.js
@@ -7,15 +7,21 @@
 //built-ins
 import { LightningElement, api, track, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
 
-//field imports
-
+import LEAD_OBJECT from '@salesforce/schema/Lead';
+//fields
 import LEAD_FIRST_NAME from '@salesforce/schema/Lead.FirstName';
 import LEAD_LAST_NAME from '@salesforce/schema/Lead.LastName';
 import LEAD_EMAIL from '@salesforce/schema/Lead.Email';
 import LEAD_HOME_PHONE from '@salesforce/schema/Lead.Phone';
 import LEAD_MOBILE_PHONE from '@salesforce/schema/Lead.MobilePhone';
 import LEAD_ADDRESS from '@salesforce/schema/Lead.Address';
+// import LEAD_STATE from '@salesforce/schema/Lead.State__c';
+
+//controller
+import getAcademicPrograms from '@salesforce/apex/c/requestForInformationFormController.getAcademicPrograms';
 
 export default class RequestForInformationForm extends LightningElement {
     @track program_type;
@@ -30,9 +36,11 @@ export default class RequestForInformationForm extends LightningElement {
     mobile_phone = LEAD_MOBILE_PHONE;
     address = LEAD_ADDRESS;
 
+    state_picklist_values;
+
+    //regex
     phone_pattern = '[0-9]{3}-[0-9]{3}-[0-9]{4}';
     email_pattern = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
-
     invalid_phone_message = 'Phone # must match format: 000-000-0000';
     invalid_email_message = 'Email must match format: example@site.com';
 
@@ -40,10 +48,21 @@ export default class RequestForInformationForm extends LightningElement {
     pageRef(result) {
         this.program_type = 'Undergraduate';
         if (result) {
-            // TO DO
-            this.program_type = 'Undergraduate'
+            console.log(JSON.stringify(result));
+            // TO DO : parse URL to get 'Undergraduate', 'Graduate', etc
         }
     }
+
+    @wire(getObjectInfo, {objectApiName: LEAD_OBJECT})
+    object_info(info) {
+        console.log(JSON.stringify(info));
+    }
+    
+
+    // @wire(getPicklistValues, {recordTypeId: , fieldApiName: LEAD_STATE})
+    // picklist_values(values) {
+    //     this.state_picklist_values = values;
+    // }
 
     get state_options() {
         return [

342dde323c6544b268a25af828742f2a804ab336
nicole-dmass <nicole.b@digitalmass.com>
2022-06-06 10:46:46 -0500

    added citizenship field w/ picklist values

--- force-app/main/default/lwc/requestForInformationForm/requestForInformationForm.js
+++ force-app/main/default/lwc/requestForInformationForm/requestForInformationForm.js
@@ -6,19 +6,12 @@
 
 //built-ins
 import { LightningElement, api, track, wire } from 'lwc';
-import { CurrentPageReference } from 'lightning/navigation';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 import { getPicklistValues } from 'lightning/uiObjectInfoApi';
 
 import LEAD_OBJECT from '@salesforce/schema/Lead';
 
 //fields
-import LEAD_FIRST_NAME from '@salesforce/schema/Lead.FirstName';
-import LEAD_LAST_NAME from '@salesforce/schema/Lead.LastName';
-import LEAD_EMAIL from '@salesforce/schema/Lead.Email';
-import LEAD_HOME_PHONE from '@salesforce/schema/Lead.Phone';
-import LEAD_MOBILE_PHONE from '@salesforce/schema/Lead.MobilePhone';
-import LEAD_ADDRESS from '@salesforce/schema/Lead.Address';
 import LEAD_CITIZENSHIP from '@salesforce/schema/Lead.hed__Citizenship__c';
 
 //controller
@@ -62,26 +55,34 @@ export default class RequestForInformationForm extends LightningElement {
 
     //regex
     phone_pattern = '[0-9]{3}-[0-9]{3}-[0-9]{4}';
-    email_pattern = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
     invalid_phone_message = 'Phone # must match format: 000-000-0000';
-    invalid_email_message = 'Email must match format: example@site.com';
 
     @wire(getRFIController, {rfi_controller_name: '$rfi_controller'})
-    rfi(controller) {
-        console.log(JSON.stringify(controller.data));
-        console.log(JSON.stringify(controller));
+    rfi(result) {
+        if (result.data) {
+            console.log(JSON.stringify(result.data));
+        } else {
+            console.log(result.error);
+        }
     }
 
     @wire(getObjectInfo, { objectApiName: LEAD_OBJECT })
     object_info(result) {
-        console.log('record type:' + JSON.stringify(result.data.defaultRecordTypeId));
-        this.lead_default_record_type = result.data.defaultRecordTypeId;
+        if (result.data) {
+            this.lead_default_record_type = result.data.defaultRecordTypeId;
+        } else {
+            console.log(result.error);
+        }
     }
 
-    @wire(getPicklistValues, { recordTypeId: this.lead_default_record_type, fieldApiName: LEAD_CITIZENSHIP })
+    @wire(getPicklistValues, { recordTypeId: '$lead_default_record_type', fieldApiName: LEAD_CITIZENSHIP })
     picklist_values(result) {
-        console.log('picklists: ' + JSON.stringify(result.data));
-        this.citizenship_picklist_values = result.data;
+        if (result.data) {
+            console.log('picklists: ' + JSON.stringify(result.data.values));
+            this.citizenship_picklist_values = result.data.values;
+        } else {
+            console.log(result.error);
+        }
     }
 
     onChange(event) {

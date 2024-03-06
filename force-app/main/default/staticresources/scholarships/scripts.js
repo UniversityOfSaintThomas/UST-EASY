ba01640173b12147728255e38c94fecc5bf4e028
Thaddaeus Dahlberg <16785863+tcdahlberg@users.noreply.github.com>
2024-03-06 08:49:52 -0600

    scholarship updates

--- force-app/main/default/staticresources/scholarships/scripts.js
+++ force-app/main/default/staticresources/scholarships/scripts.js
@@ -4,18 +4,29 @@ let ready = (callback) => {
 }
 
 ready(() => {
-    document.querySelectorAll("#e1upload, #e1input, #e2upload, #e2input").forEach(function (el) {
-        el.style.display = "none";
-    });
 
     document.querySelectorAll(".essayInputWrapper").forEach(function (essayWrap) {
-        essayWrap.querySelectorAll(".essayTypeSelection").forEach(function(essayType) {
+        let essayNo = essayWrap.dataset.essayno;
+        essayWrap.querySelectorAll(".essayTypeSelection").forEach(function (essayType) {
+            let uploaded = essayInfo['essayUploaded' + essayNo];
+            let textInput = essayInfo['essayInput' + essayNo];
+            essayWrap.querySelector(".essayInput").style.display = "none";
+            essayWrap.querySelector(".essayUpload").style.display = "none"
+            if (uploaded) {
+                essayType.value = "upload";
+                essayWrap.querySelector(".essayUpload").style.display = "block";
+            } else if (textInput) {
+                essayType.value = "input";
+                essayWrap.querySelector(".essayInput").style.display = "block";
+            }
             essayType.addEventListener("change", function () {
                 let selectedType = this.value;
-                if(selectedType === 'upload') {
+                if (selectedType === 'upload') {
+                    essayInfo['essayUploaded' + essayNo] = true;
                     essayWrap.querySelector(".essayInput").style.display = "none";
                     essayWrap.querySelector(".essayUpload").style.display = "block";
                 } else if (selectedType === 'input') {
+                    essayInfo['essayInput' + essayNo] = true;
                     essayWrap.querySelector(".essayUpload").style.display = "none";
                     essayWrap.querySelector(".essayInput").style.display = "block";
                 }
@@ -44,50 +55,54 @@ ready(() => {
 });
 
 function validateCriteria() {
-    let alertMessage = "", testScore = 0, gpa = 0.00;
-    removePreviousErrors();
+
     let declaredTest = document.querySelector("[id$=Declared_Test]");
     if (declaredTest) {
-        testScore = declaredTest.value;
-        if (testScore < 10) {
-            alertMessage += "<li>Please enter a valid ACT/SAT score (example 27).</li>";
-            declaredTest.classList.add("validationError");
+        let formWrap = declaredTest.closest(".slds-form-element");
+        let hepText = formWrap.querySelector(".slds-form-element__help");
+        if (declaredTest.value < 10) {
+            formWrap.classList.add("slds-has-error");
         }
+        declaredTest.addEventListener("change", function () {
+            formWrap.classList.remove("slds-has-error");
+        });
     }
 
     let declaredGPA = document.querySelector("[id$=Declared_GPA]");
     if (declaredGPA) {
-        gpa = declaredGPA.value;
-        if (gpa < 1.0) {
-            alertMessage += "<li>Please enter a valid gpa (example 3.5).</li>";
-            declaredGPA.classList.add("validationError");
+        let formWrap = declaredGPA.closest(".slds-form-element");
+        let hepText = formWrap.querySelector(".slds-form-element__help");
+        if (declaredGPA.value < 1.0) {
+            formWrap.classList.add("slds-has-error");
         }
+        declaredGPA.addEventListener("change", function () {
+            formWrap.classList.remove("slds-has-error");
+        });
     }
 
     let criteriaChecked = document.querySelector("[id$=confirmCriteria]");
     if (criteriaChecked) {
+        let formWrap = criteriaChecked.closest(".slds-form-element");
+        let hepText = formWrap.querySelector(".slds-form-element__help");
         if (!criteriaChecked.checked) {
-            alertMessage += "<li>You must acknowledge you understand and meet the criteria outlined on this page to proceed. Signify this by checking the confirmation checkbox.</li>";
-            criteriaChecked.closest(".slds-form-element").querySelector("label").classList.add("validationError");
+            formWrap.classList.add("slds-has-error");
         }
+        criteriaChecked.addEventListener("change", function () {
+            if (criteriaChecked.checked) {
+                formWrap.classList.remove("slds-has-error");
+            }
+        });
     }
 
-    if (alertMessage != "") {
-        let errorMessages = document.querySelector(".errorMessages");
-        let errorList = document.createElement("div");
-        errorMessages.style.display = "block";
-        errorList.id = "validationErrorList";
-        errorList.innerHTML = "<ul>" + alertMessage + "</ul>";
-        errorMessages.appendChild(errorList);
-        document.querySelector('.errorMessages').scrollIntoView({behavior: 'smooth'});
-        return false;
+    if (document.querySelectorAll(".slds-has-error").length > 0) {
+        return false
     }
     return true;
 }
 
-function checkForfile(fileSelctor) {
+function checkForFile(fileSelector) {
     removePreviousErrors();
-    let file = document.querySelector("[id$=" + fileSelctor + "]");
+    let file = document.querySelector("[id$=" + fileSelector + "]");
     if (file) {
         if (file.value) {
             return true;
@@ -106,22 +121,24 @@ function checkForfile(fileSelctor) {
     return false;
 }
 
-function validateRecommender(recId) {
-    let recInfo = document.querySelector("[id$=" + recId + "]").closest(".recommender");
-    let recEmail = recInfo.find(".recEmail");
-    let recError = recInfo.find(".errorMessages2");
-    let recName = recInfo.find(".recName");
-    recEmail.classList.remove("validationError");
-    recName.classList.remove("validationError");
-    recError.hide();
-    if (!isValidEmailAddress(recEmail.value) || !recName.value) {
-        recError.innerHTML("Please enter a name and a valid email.").show();
-        recEmail.classList.add("validationError");
-        recName.classList.add("validationError");
-        return false;
-    }
-    return true;
-}
+//
+// function validateRecommender(recId) {
+//     alert(recId);
+//     let recInfo = document.getElementById(recId).closest(".recommender");
+//     let recEmail = recInfo.find(".recEmail");
+//     let recError = recInfo.find(".errorMessages2");
+//     let recName = recInfo.find(".recName");
+//     recEmail.classList.remove("validationError");
+//     recName.classList.remove("validationError");
+//     recError.hide();
+//     if (!isValidEmailAddress(recEmail.value) || !recName.value) {
+//         recError.innerHTML("Please enter a name and a valid email.").show();
+//         recEmail.classList.add("validationError");
+//         recName.classList.add("validationError");
+//         return false;
+//     }
+//     return true;
+// }
 
 function isValidEmailAddress(emailAddress) {
     const pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
@@ -129,67 +146,57 @@ function isValidEmailAddress(emailAddress) {
 }
 
 function validateApplication() {
-    let alertMessage = "", essaySelector = "";
-    removePreviousErrors();
 
-    let hasError = false;
-    document.querySelectorAll('.ScholarshipAppForm input, .ScholarshipAppForm textarea:not(.essayInput)').forEach(function (input) {
-        if (!input.value) {
-            hasError = true;
-            input.classList.add("validationError");
+    document.querySelectorAll(".ScholarshipAppForm input[type=text], .ScholarshipAppForm textarea").forEach(function (formInput) {
+        if (formInput) {
+            if (!formInput.value) {
+                let inputWrap = formInput.closest(".slds-form-element");
+                if (inputWrap) {
+                    if (inputWrap.classList.contains("slds-is-required")) {
+                        inputWrap.classList.add("slds-has-error");
+                        formInput.addEventListener("change", function () {
+                            inputWrap.classList.remove("slds-has-error");
+                        });
+                    }
+                }
+            }
         }
     });
-    if (hasError) {
-        alertMessage += "<li>All questions require an answer. Please fill in your answers for the highlighted text areas.</li>";
+
+    //Check for CKEditor content
+    let richQuerySelector = [];
+    if (essayInfo['essayInput1']) {
+        richQuerySelector.add(".essay1");
     }
+    if (essayInfo['essayInput2']) {
+        richQuerySelector.add(".essay2");
+    }
+    richQuerySelector = richQuerySelector.join(", ");
+    if (richQuerySelector) {
+        document.querySelectorAll(richQuerySelector).forEach(function (editor) {
+            essayInfo['essayInput1'];
+
+            if (CKEDITOR.instances[editor.id].getData() === "") {
+                let inputWrap = editor.closest(".slds-form-element");
+                inputWrap.classList.add("slds-has-error");
+                CKEDITOR.instances[editor.id].on("change", function () {
+                    inputWrap.classList.remove("slds-has-error");
+                });
+            }
+        });
+    }
+
+
+    if (document.querySelectorAll(".slds-has-error").length > 0) {
+        return false
+    }
+    return true;
 
     document.querySelectorAll(".essay1input, .essay2input").forEach(function (essay) {
         alert(this.value);
-
     });
-
-    //Check Essay for input
-    /*    if ($(".essay1input").length > 0) {
-            if ($(".essay1Uploaded").length === 0) {
-                let essay1Id = $(".essay1").attr("id");
-                console.log(essay1Id);
-                if (!CKEDITOR.instances[$(".essay1").attr("id")].getData()) {
-                    alertMessage += "<li>Please input an answer to essay one.</li>";
-                    $(".essay1input").addClass("validationErrorRich");
-                }
-            }
-        }
-
-        if ($(".essay2input").length > 0) {
-            if ($(".essay2Uploaded").length === 0) {
-                if (!CKEDITOR.instances[$(".essay2").attr("id")].getData()) {
-                    alertMessage += "<li>Please input an answer to essay two.</li>";
-                    $(".essay2input").addClass("validationErrorRich");
-                }
-            }
-        }*/
-
-
-    if (alertMessage != "") {
-        let errorMessages = document.querySelector(".errorMessages");
-        errorMessages.style.display = "block";
-        errorMessages.innerHTML = errorMessages.innerHTML + "<div id='validationErrorList'><ul>" + alertMessage + "</ul></div>";
-        document.querySelector('.errorMessages').scrollIntoView({behavior: 'smooth'});
-        return false;
-    }
-    return true;
 }
 
-function displayError(error) {
-    let errorMessages = document.querySelector(".errorMessages");
-    let errorList = document.createElement("div");
-    errorMessages.style.display = "block";
-    errorList.id = "validationErrorList";
-    errorList.innerHTML = "<ul>" + alertMessage + "</ul>";
-    errorMessages.appendChild(errorList);
-    document.querySelector('.errorMessages').scrollIntoView({behavior: 'smooth'});
-    return false;
-}
 
 function removePreviousErrors() {
     document.querySelectorAll(".validationError").forEach(function (err) {

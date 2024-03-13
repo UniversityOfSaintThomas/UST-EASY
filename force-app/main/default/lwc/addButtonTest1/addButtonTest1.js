840891f81a2a019e11d000b23bc3bcf30322fbca
ynguyen-it <149885979+ynguyen-it@users.noreply.github.com>
2024-03-12 19:55:26 -0500

    Update

--- force-app/main/default/lwc/addButtonTest1/addButtonTest1.js
+++ force-app/main/default/lwc/addButtonTest1/addButtonTest1.js
@@ -17,7 +17,7 @@ export default class AddButtonTest1 extends LightningElement {
             label: 'Button Label',
             fieldName: 'buttonLabel',
             type: 'text',
-            initialWidth: 200,
+            initialWidth: 125,
             wrapText: true,
             hideDefaultActions: true
         },
@@ -25,14 +25,38 @@ export default class AddButtonTest1 extends LightningElement {
             label: 'Button Link',
             fieldName: 'buttonLink',
             type: 'text',
-            initialWidth: 575,
+            initialWidth: 430,
             wrapText: true,
             hideDefaultActions: true
         },
         {
-            label: 'Move Up',
+            label: 'Style Code',
+            fieldName: 'buttonStyle',
+            type: 'text',
+            initialWidth: 95,
+            wrapText: true,
+            hideDefaultActions: true
+        },
+        {
+            label: 'Size Code',
+            fieldName: 'buttonSize',
+            type: 'text',
+            initialWidth: 90,
+            wrapText: true,
+            hideDefaultActions: true
+        },
+        {
+            label: 'Position Code',
+            fieldName: 'buttonPosition',
+            type: 'text',
+            initialWidth: 115,
+            wrapText: true,
+            hideDefaultActions: true
+        },
+        {
+            label: '',
             type: "button-icon",
-            initialWidth: 75,
+            // initialWidth: 45,
             wrapText: true,
             typeAttributes: {
                 name: 'up',
@@ -43,9 +67,9 @@ export default class AddButtonTest1 extends LightningElement {
             }
         },
         {
-            label: 'Move Down',
+            label: '',
             type: "button-icon",
-            initialWidth: 90,
+            // initialWidth: 45,
             wrapText: true,
             typeAttributes: {
                 name: 'down',
@@ -56,9 +80,9 @@ export default class AddButtonTest1 extends LightningElement {
             }
         },
         {
-            label: 'Delete',
+            label: '',
             type: "button-icon",
-            initialWidth: 60,
+            // initialWidth: 45,
             wrapText: true,
             typeAttributes: {
                 name: 'delete',
@@ -70,17 +94,49 @@ export default class AddButtonTest1 extends LightningElement {
         }
     ];
 
+    get buttonStyleOptions() {
+        return [
+            { label: 'No Style', value: 'slds-button_no_style' }, //slds-button_no_style does not exist. Using to fulfill field value requirement check.
+            { label: 'Brand', value: 'slds-button_brand' },
+            { label: 'Outline Brand Button', value: 'slds-button_outline-brand' },
+            { label: 'Neutral', value: 'slds-button_neutral' },
+        ];
+    }
+
+    get buttonSizeOptions() {
+        return [
+            { label: 'Small: 100px', value: '100px' },
+            { label: 'Medium: 200px', value: '200px' },
+            { label: 'Large: 300px', value: '300px' },
+            { label: 'x-Large: 400px', value: '400px' },
+        ];
+    }
+
+    get buttonPositionOptions() {
+        return [
+            { label: 'Left', value: 'slds-grid_align-left' }, //Left position is default/no class. slds-grid_align-left does not exist. Using to fulfill field value requirement check.
+            { label: 'Center', value: 'slds-grid_align-center' },
+            { label: 'Right', value: 'slds-grid_align-end' },
+        ];
+    }
+
     showDataTable = false;
 
     @api recordId;
 
     @track button_label;
     @track button_link;
+    @track button_style;
+    @track button_size;
+    @track button_position;
     @track buttonJSON = [];
     @track buttonItem = {
         'id': '',
         'buttonLabel': '',
         'buttonLink': '',
+        'buttonStyle': '',
+        'buttonSize': '',
+        'buttonPosition': '',
     };
 
     @wire(getRecord, {recordId: '$recordId', fields: WIDGETFIELDS})
@@ -107,7 +163,7 @@ export default class AddButtonTest1 extends LightningElement {
     validInput(event) {
 
         const allValid = [
-            ...this.template.querySelectorAll('lightning-input')
+            ...this.template.querySelectorAll('lightning-input, lightning-combobox')
         ].reduce((validSoFar, inputCmp) => {
 
             inputCmp.reportValidity();
@@ -145,6 +201,21 @@ export default class AddButtonTest1 extends LightningElement {
                 this.buttonItem['buttonLink'] = targetValue;
 
                 break;
+            case 'ButtonStyle':
+
+                this.buttonItem['buttonStyle'] = targetValue;
+
+                break;
+            case 'ButtonSize':
+
+                this.buttonItem['buttonSize'] = targetValue;
+
+                break;
+            case 'ButtonPosition':
+
+                this.buttonItem['buttonPosition'] = targetValue;
+
+                break;
             case 'Submit':
 
                 this.buttonJSON.push(this.buttonItem);
@@ -161,7 +232,7 @@ export default class AddButtonTest1 extends LightningElement {
                 //Reset form
                 this.template.querySelector("form").reset();
 
-                this.template.querySelectorAll('form lightning-input').forEach(each => {
+                this.template.querySelectorAll('form, lightning-input, lightning-combobox').forEach(each => {
                     each.value = undefined;
                 });
 
@@ -169,6 +240,9 @@ export default class AddButtonTest1 extends LightningElement {
                     'id': '',
                     'buttonLabel': '',
                     'buttonLink': '',
+                    'buttonStyle': '',
+                    'buttonSize': '',
+                    'buttonPosition': '',
                 };
 
                 break;

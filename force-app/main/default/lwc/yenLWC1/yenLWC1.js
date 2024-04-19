9b3efa6a240eb18ab8828a2158df5bb84005d407
ynguyen-it <149885979+ynguyen-it@users.noreply.github.com>
2024-04-19 16:57:07 -0500

    update

--- force-app/main/default/lwc/yenLWC1/yenLWC1.js
+++ force-app/main/default/lwc/yenLWC1/yenLWC1.js
@@ -2,10 +2,39 @@
  * Created by nguy0092 on 4/15/2024.
  */
 
-import {LightningElement,api} from 'lwc';
+import {LightningElement, api, wire} from 'lwc';
+import eventRegistrationList from "@salesforce/apex/YnSummitEventsList.eventsRegistrations";
+
+const EVENTREGISTRATIONCOLUMNS = [
+    // {label: 'Id', fieldName: 'Id'},
+    {label: 'Event Name', fieldName: 'EventName'},
+    {label: 'Event Date Time', fieldName: 'EventDateTime'}
+];
 
 export default class YenLwc1 extends LightningElement {
 
-    @api helloId;
+    // registrationList;
+    registrationListError;
+    eventRegistrationColumns = EVENTREGISTRATIONCOLUMNS;
+
+    @api contactId = '00303000013EvRqAAK';
+
+    @wire(eventRegistrationList, {contactId: '$contactId'})
+    registrationList
+    // registrations(result) {
+    //
+    //     if (result.data) {
+    //
+    //         this.registrationList = result.data;
+    //
+    //
+    //     } else if (result.error) {
+    //
+    //         this.registrationListError = result.error;
+    //         this.registrationList = undefined;
+    //
+    //     }
+    //
+    // }
 
 }

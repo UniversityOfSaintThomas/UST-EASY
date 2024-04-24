38a07b97c6ba3a9227712c4d8efb75d260628e77
ynguyen-it <149885979+ynguyen-it@users.noreply.github.com>
2024-04-24 17:18:33 -0500

    update

--- force-app/main/default/lwc/summitEventsListView/summitEventsListView.js
+++ force-app/main/default/lwc/summitEventsListView/summitEventsListView.js
@@ -1,49 +1,11 @@
 /**
  * Created by nguy0092 on 4/17/2024.
+ * Displays Summit Event Registration in Easy Widget.
+ * Aura app helper: summitEventsListApp
  */
 
 import {LightningElement, api, wire} from 'lwc';
 import {gql, graphql} from "lightning/uiGraphQLApi";
-
-// // const columns = [
-//     {
-//         label: "Event Instance",
-//         fieldName: "EventInstance"
-//     },
-//     {
-//         label: "Event Name",
-//         fieldName: "EventName"
-//     },
-//     {
-//         label: "Event Date",
-//         fieldName: "EventDate",
-//         type: "text"
-//         // typeAttributes: {
-//         //     weekday: "long",
-//         //     year: "numeric",
-//         //     month: "long",
-//         //     day: "2-digit"
-//         // }
-//     },
-//     {
-//         label: "Event Time",
-//         fieldName: "EventTime",
-//         type: "text"
-//         // typeAttributes: {
-//         //     hour: "2-digit",
-//         //     minute: "2-digit"
-//         // }
-//     },
-//     {
-//         label: "Event Date & Time",
-//         fieldName: "EventDateTime",
-//         type: "text"
-//         // typeAttributes: {
-//         //     hour: "2-digit",
-//         //     minute: "2-digit"
-//         // }
-//     },
-// ];
 
 export default class SummitEventsListView extends LightningElement {
 
@@ -51,21 +13,7 @@ export default class SummitEventsListView extends LightningElement {
     errors;
 
     @api contactId;
-    // @api dateFilter = new Date();
-
-    // columns = columns;
-
-    // dateOptions = {
-    //     weekday: 'long',
-    //     year: 'numeric',
-    //     month: 'long',
-    //     day: 'numeric'
-    // };
-    //
-    // timeOptions = {
-    //     hour: "2-digit",
-    //     minute: "2-digit"
-    // };
+    // @api dateFilter
 
     @wire(graphql, {
         query: gql`
@@ -74,6 +22,7 @@ export default class SummitEventsListView extends LightningElement {
               query {
                 summit__Summit_Events_Registration__c ( where: { summit__Contact__c: { eq: $contactId } 
                                                                  summit__Event_Instance__r: { summit__Instance_Start_Date__c: { gte: {literal: TODAY } } } 
+                                                                 summit__Status__c: { in: ["Registered", "Confirmed"] }
                                                                },
                                                         orderBy: { summit__Event_Instance__r: { summit__Instance_Start_Date__c: { order: ASC } } 
                                                                  } 
@@ -81,7 +30,7 @@ export default class SummitEventsListView extends LightningElement {
                     {
                   edges {
                     node {
-                      Id
+                      Id                   
                       Name {value}
                       summit__Event_Name__c {value}
                       summit__Event_Instance__c {value}
@@ -89,6 +38,8 @@ export default class SummitEventsListView extends LightningElement {
                       summit__Event_Instance__r { summit__Instance_Start_Time__c {value} }
                       summit__Event_Instance__r { summit__Instance_End_Time__c {value} }
                       summit__Event_Instance__r { summit__Registration_Link__c {value} }
+                      summit__Contact__c {value}
+                      summit__Status__c {value}
                     }
                   }
                 }
@@ -97,7 +48,7 @@ export default class SummitEventsListView extends LightningElement {
           }
         `,
         variables: "$variables",
-    })
+    } )
     graphqlQueryResult({data, errors}) {
         if (data) {
             // this.results = data.uiapi.query.summit__Summit_Events_Registration__c.edges.map((edge) => edge.node);
@@ -107,12 +58,6 @@ export default class SummitEventsListView extends LightningElement {
                 EventInstance: edge.node.summit__Event_Instance__c.value,
                 EventStartDate: edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value,
                 EventStartDateTime: edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value,
-                // EventDate: new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value.slice(0, -8)).toLocaleDateString("en-US", this.dateOptions),
-                // EventTime: new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value.slice(0, -8)).toLocaleTimeString("en-US", this.timeOptions) + ' - ' +
-                //     new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_End_Time__c.value.slice(0, -8)).toLocaleTimeString("en-US", this.timeOptions),
-                // EventDateTime: new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value.slice(0, -8)).toLocaleDateString("en-US", this.dateOptions) + ' ' +
-                //     new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value.slice(0, -8)).toLocaleTimeString("en-US", this.timeOptions) + ' - ' +
-                //     new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_End_Time__c.value.slice(0, -2)).toLocaleTimeString("en-US", this.timeOptions),
                 EventRegistrationLink: edge.node.summit__Event_Instance__r.summit__Registration_Link__c.value.split("\"")[1],
             }));
         }

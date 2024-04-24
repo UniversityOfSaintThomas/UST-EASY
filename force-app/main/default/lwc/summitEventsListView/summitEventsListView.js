481a2287b5c66c07427eeefe6fb48d54a2634b83
ynguyen-it <149885979+ynguyen-it@users.noreply.github.com>
2024-04-23 21:54:20 -0500

    update

--- force-app/main/default/lwc/summitEventsListView/summitEventsListView.js
+++ force-app/main/default/lwc/summitEventsListView/summitEventsListView.js
@@ -5,45 +5,45 @@
 import {LightningElement, api, wire} from 'lwc';
 import {gql, graphql} from "lightning/uiGraphQLApi";
 
-const columns = [
-    // {
-    //     label: "Event Instance",
-    //     fieldName: "EventInstance"
-    // },
-    {
-        label: "Event Name",
-        fieldName: "EventName"
-    },
-    {
-        label: "Event Date",
-        fieldName: "EventDate",
-        type: "text"
-        // typeAttributes: {
-        //     weekday: "long",
-        //     year: "numeric",
-        //     month: "long",
-        //     day: "2-digit"
-        // }
-    },
-    {
-        label: "Event Time",
-        fieldName: "EventTime",
-        type: "text"
-        // typeAttributes: {
-        //     hour: "2-digit",
-        //     minute: "2-digit"
-        // }
-    },
-    {
-        label: "Event Date & Time",
-        fieldName: "EventDateTime",
-        type: "text"
-        // typeAttributes: {
-        //     hour: "2-digit",
-        //     minute: "2-digit"
-        // }
-    },
-];
+// // const columns = [
+//     {
+//         label: "Event Instance",
+//         fieldName: "EventInstance"
+//     },
+//     {
+//         label: "Event Name",
+//         fieldName: "EventName"
+//     },
+//     {
+//         label: "Event Date",
+//         fieldName: "EventDate",
+//         type: "text"
+//         // typeAttributes: {
+//         //     weekday: "long",
+//         //     year: "numeric",
+//         //     month: "long",
+//         //     day: "2-digit"
+//         // }
+//     },
+//     {
+//         label: "Event Time",
+//         fieldName: "EventTime",
+//         type: "text"
+//         // typeAttributes: {
+//         //     hour: "2-digit",
+//         //     minute: "2-digit"
+//         // }
+//     },
+//     {
+//         label: "Event Date & Time",
+//         fieldName: "EventDateTime",
+//         type: "text"
+//         // typeAttributes: {
+//         //     hour: "2-digit",
+//         //     minute: "2-digit"
+//         // }
+//     },
+// ];
 
 export default class SummitEventsListView extends LightningElement {
 
@@ -51,27 +51,34 @@ export default class SummitEventsListView extends LightningElement {
     errors;
 
     @api contactId;
+    // @api dateFilter = new Date();
 
-    columns = columns;
+    // columns = columns;
 
-    dateOptions = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    };
-
-    timeOptions = {
-        hour: "2-digit",
-        minute: "2-digit"
-    };
+    // dateOptions = {
+    //     weekday: 'long',
+    //     year: 'numeric',
+    //     month: 'long',
+    //     day: 'numeric'
+    // };
+    //
+    // timeOptions = {
+    //     hour: "2-digit",
+    //     minute: "2-digit"
+    // };
 
     @wire(graphql, {
         query: gql`
           query SummitEventRegistrations ($contactId: ID) {
             uiapi {
               query {
-                summit__Summit_Events_Registration__c (where: {summit__Contact__c: { eq: $contactId } }) {
+                summit__Summit_Events_Registration__c ( where: { summit__Contact__c: { eq: $contactId } 
+                                                                 summit__Event_Instance__r: { summit__Instance_Start_Date__c: { gte: {literal: TODAY } } } 
+                                                               },
+                                                        orderBy: { summit__Event_Instance__r: { summit__Instance_Start_Date__c: { order: ASC } } 
+                                                                 } 
+                                                       ) 
+                    {
                   edges {
                     node {
                       Id
@@ -81,6 +88,7 @@ export default class SummitEventsListView extends LightningElement {
                       summit__Event_Instance__r { summit__Instance_Start_Date__c {value} }
                       summit__Event_Instance__r { summit__Instance_Start_Time__c {value} }
                       summit__Event_Instance__r { summit__Instance_End_Time__c {value} }
+                      summit__Event_Instance__r { summit__Registration_Link__c {value} }
                     }
                   }
                 }
@@ -97,12 +105,15 @@ export default class SummitEventsListView extends LightningElement {
                 Id: edge.node.Id,
                 EventName: edge.node.summit__Event_Name__c.value,
                 EventInstance: edge.node.summit__Event_Instance__c.value,
-                EventDate: new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value.slice(0, -8)).toLocaleDateString("en-US", this.dateOptions),
-                EventTime: new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value.slice(0, -8)).toLocaleTimeString("en-US", this.timeOptions) + ' - ' +
-                    new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_End_Time__c.value.slice(0, -8)).toLocaleTimeString("en-US", this.timeOptions),
-                EventDateTime: new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value.slice(0, -8)).toLocaleDateString("en-US", this.dateOptions) + ' ' +
-                    new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value.slice(0, -8)).toLocaleTimeString("en-US", this.timeOptions) + ' - ' +
-                    new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_End_Time__c.value.slice(0, -2)).toLocaleTimeString("en-US", this.timeOptions)
+                EventStartDate: edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value,
+                EventStartDateTime: edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value,
+                // EventDate: new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value.slice(0, -8)).toLocaleDateString("en-US", this.dateOptions),
+                // EventTime: new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value.slice(0, -8)).toLocaleTimeString("en-US", this.timeOptions) + ' - ' +
+                //     new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_End_Time__c.value.slice(0, -8)).toLocaleTimeString("en-US", this.timeOptions),
+                // EventDateTime: new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value.slice(0, -8)).toLocaleDateString("en-US", this.dateOptions) + ' ' +
+                //     new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_Start_Time__c.value.slice(0, -8)).toLocaleTimeString("en-US", this.timeOptions) + ' - ' +
+                //     new Date(edge.node.summit__Event_Instance__r.summit__Instance_Start_Date__c.value + 'T' + edge.node.summit__Event_Instance__r.summit__Instance_End_Time__c.value.slice(0, -2)).toLocaleTimeString("en-US", this.timeOptions),
+                EventRegistrationLink: edge.node.summit__Event_Instance__r.summit__Registration_Link__c.value.split("\"")[1],
             }));
         }
         this.errors = errors;
@@ -111,6 +122,7 @@ export default class SummitEventsListView extends LightningElement {
     get variables() {
         return {
             contactId: this.contactId,
+            // dateFilter: this.dateFilter,
 
         }
     }
--- force-app/main/default/lwc/yenLWC1/yenLWC1.js
+++ force-app/main/default/lwc/yenLWC1/yenLWC1.js
@@ -4,23 +4,37 @@
 
 import {LightningElement, api, wire} from 'lwc';
 import eventRegistrationList from "@salesforce/apex/YnSummitEventsList.eventsRegistrations";
+// import eventRegistrationListTest from "@salesforce/apex/YnSummitEventsListTEST1.eventsRegistrations";
 
-const EVENTREGISTRATIONCOLUMNS = [
-    // {label: 'Id', fieldName: 'Id'},
-    {label: 'Event Name', fieldName: 'EventName'},
-    {label: 'Event Date Time', fieldName: 'EventDateTime'}
-];
+// const EVENTREGISTRATIONCOLUMNS = [
+//     // {label: 'Id', fieldName: 'Id'},
+//     {label: 'Event Name', fieldName: 'EventName'},
+//     {label: 'Event Date Time', fieldName: 'EventDateTime'}
+// ];
 
 export default class YenLwc1 extends LightningElement {
 
     // registrationList;
-    registrationListError;
-    eventRegistrationColumns = EVENTREGISTRATIONCOLUMNS;
 
-    @api contactId = '00303000013EvRqAAK';
+    // @wire(eventRegistrationListTest, {})
+    // registrations
+    // registrations({ error, data }) {
+    //     if (data) {
+    //         //note you'll need to change your template to handle this - remove the `data` object when you access activities
+    //         this.registrationList = data;
+    //     } else if (error) {
+    //         console.log("this is error: "+error);
+    //     }
+    // }
+
+    // registrationListError;
+    // eventRegistrationColumns = EVENTREGISTRATIONCOLUMNS;
+
+    @api contactId = '00303000013FeEdAAK';
 
     @wire(eventRegistrationList, {contactId: '$contactId'})
     registrationList
+
     // registrations(result) {
     //
     //     if (result.data) {
